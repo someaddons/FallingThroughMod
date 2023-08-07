22c40f91b2e7639f62cb787da7877a1d541cc04a
someaddons <someaddons@github.com>
2023-08-07 21:05:16 +0200

    4.1: add cupboard config

--- src/main/java/com/fallingthrough/FallingthroughMod.java
+++ src/main/java/com/fallingthrough/FallingthroughMod.java
@@ -1,6 +1,7 @@
 package com.fallingthrough;
 
-import com.fallingthrough.config.Configuration;
+import com.cupboard.config.CupboardConfig;
+import com.fallingthrough.config.CommonConfiguration;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,10 +11,10 @@ import java.util.Random;
 // The value here should match an entry in the META-INF/mods.toml file
 public class FallingthroughMod implements ModInitializer
 {
-    public static final String        MODID  = "forgivingworld";
-    public static final Logger        LOGGER = LogManager.getLogger();
-    public static       Configuration config = new Configuration();
-    public static       Random        rand   = new Random();
+    public static final String                              MODID  = "forgivingworld";
+    public static final Logger                              LOGGER = LogManager.getLogger();
+    public static       CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MODID, new CommonConfiguration());
+    public static       Random                              rand   = new Random();
 
     public FallingthroughMod()
     {
